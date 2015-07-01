f8ca9223f3b18f64ee67649396ed13945c608aaf
RHEA GANAPATHY <rheasg@gmail.com>
2015-07-01 19:02:20 -0400

    Writing the review routes, controllers and views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,10 +5,15 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+
+    if current_user
+      @review = @product.reviews.build
+    end
   end
 
   def new
     @product = Product.new
+    render "new"
   end
 
   def edit
